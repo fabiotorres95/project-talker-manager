305c4aac72d134cd3530f181dcee66ad3d44688f
fabiotorres95 <engfabiotorres@outlook.com>
2023-06-19 16:10:26 -0300

    initial commit + created fsUtils

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const readTalkerData = require('./utils/fsUtils');
 
 const app = express();
 app.use(express.json());
@@ -13,4 +14,9 @@ app.get('/', (_request, response) => {
 
 app.listen(PORT, () => {
   console.log('Online');
+});
+
+app.get('/talker', async (_req, res) => {
+  const talkers = await readTalkerData();
+  return res.status(HTTP_OK_STATUS).json(talkers);
 });
---
+++ src/utils/fsUtils.js
@@ -0,0 +1,11 @@
+async function readTalkerData() {
+  try {
+    console.log('feito');
+  } catch (err) {
+    console.log('não feito');
+  }
+}
+
+module.exports = {
+  readTalkerData,
+};
